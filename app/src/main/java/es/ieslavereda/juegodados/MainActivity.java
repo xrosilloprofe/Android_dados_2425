9eb1b4fabb8487ce4d8857d9589f6fea143148ab
xrosilloprofe <xv.rosillo@edu.gva.es>
2025-03-31 20:00:20 +0200

    20240331

--- app/src/main/java/es/ieslavereda/juegodados/MainActivity.java
+++ app/src/main/java/es/ieslavereda/juegodados/MainActivity.java
@@ -3,6 +3,7 @@ package es.ieslavereda.juegodados;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,7 +18,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button lanzarButton;
+    private Button lanzarButton, lanzarTodosButton;
+    private ImageButton imageButton1, imageButton2, imageButton3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,17 +34,46 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageView);
         lanzarButton = findViewById(R.id.lanzarButton);
-//        imageView.setImageResource(R.mipmap.ic_cuatro_foreground);
-        Random numeroRandom = new Random();
+        lanzarTodosButton = findViewById(R.id.todosDadosButton);
+        imageButton1 = findViewById(R.id.imageButton1);
+        imageButton2 = findViewById(R.id.imageButton2);
+        imageButton3 = findViewById(R.id.imageButton3);
+
+
 
         lanzarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int numero = numeroRandom.nextInt(6);
-                imageView.setImageResource(Dado.values()[numero].getImagen());
+                imageView.setImageResource(Dado.values()[numeroAzar()].getImagen());
             }
         });
 
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageButton1.setImageResource(Dado.values()[numeroAzar()].getImagen());
+            }
+        });
+
+        imageButton2.setOnClickListener(view -> {
+            imageButton2.setImageResource(Dado.values()[numeroAzar()].getImagen());
+        });
+
+        imageButton3.setOnClickListener(view -> {
+            imageButton3.setImageResource(Dado.values()[numeroAzar()].getImagen());
+        });
+
+        lanzarTodosButton.setOnClickListener(view -> {
+            imageButton1.callOnClick();
+            imageButton2.setImageResource(Dado.values()[numeroAzar()].getImagen());
+            imageButton3.callOnClick();
+        });
 
     }
+
+    private int numeroAzar(){
+        Random numeroRandom = new Random();
+        return numeroRandom.nextInt(6);
+    }
+
 }
